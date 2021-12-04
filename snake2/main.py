e1e53bea783f5466c255223d555e09d91612082d
Wilson Li <wilsonli.cn@gmail.com>
2021-12-04 11:43:34 -0800

    snake2

---
+++ snake2/main.py
@@ -0,0 +1,11 @@
+import pygame
+
+#init
+pygame.init()
+dis=pygame.display.set_mode((400,300))
+pygame.display.update()
+pygame.display.set_caption('Snake game by Wilson')
+
+#quit
+pygame.quit()
+quit()
